4ef2acc30053cb516c54bc33a20ea4404e2ff84c
loyti <brice@loyti.com>
2017-09-07 15:05:33 -0700

    updates: bullets firing

--- cmHack/GameViewController.swift
+++ cmHack/GameViewController.swift
@@ -34,6 +34,19 @@ class GameViewController: UIViewController {
             view.showsNodeCount = true
         }
         
+        // Configure the view.
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
+        
+        // Create and configure the scene.
+        let scene = GameScene(size: skView.frame.size)
+        skView.presentScene(scene)
+        
+        
         motionManager = CMMotionManager()
         if let manager = motionManager {
             print("Motion manager is active")
